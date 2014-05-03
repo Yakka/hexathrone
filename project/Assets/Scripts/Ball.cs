a1534118314d93047dfc9ba4ae491ed26a45eee5
Emmanuel Corno <emmanuel.corno@neuf.fr>
2014-05-03 18:12:42 +0200

    Power ups can be catched by pressing SPACE

--- project/Assets/Scripts/Ball.cs
+++ project/Assets/Scripts/Ball.cs
@@ -69,6 +69,27 @@ public class Ball : MonoBehaviour {
 			triggerGate = true;
 		}
 
+		
+
+    }
+
+    void OnTriggerStay2D(Collider2D other) {
+    	if (other.gameObject.name == "PowerUp") {
+			bool destroy = false;
+			#if UNITY_ANDROID
+				//TEMP Code !!
+				if(Input.GetKey(KeyCode.Space)) {
+					destroy = true;
+				}
+				// Swipe!
+			#else
+				// COPY PASTE HERE FOR WINDOWS BUILD
+			#endif
+
+			if(destroy)
+				Destroy(other.gameObject);
+
+		}
     }
 
 	void OnTriggerExit2D(Collider2D other) {
