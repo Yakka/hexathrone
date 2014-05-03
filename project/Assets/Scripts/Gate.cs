781c6b20d7cf95101960d51d70d7c61e0989b9d2
Vincent <vincentiu.adam@gmail.com>
2014-05-03 23:04:57 +0200

    Added Gates
    Added Input Manager
    
    Can pass through gates :D

--- project/Assets/Scripts/Gate.cs
+++ project/Assets/Scripts/Gate.cs
@@ -1,9 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Gate : MonoBehaviour {
+public class Gate : MonoBehaviour 
+{
+	//consider the gate opens to the right otherwise
+	public bool m_isLeftTrigger;
+	public bool m_isTriggerPressed;
 
-	private bool isTrigger = false;
+	public float m_wallColliderDelay;
+	public float m_triggerDelay;
+
+	public Collider2D[] m_colliders;
+
+	private float m_triggerCounter;
 
 	// Use this for initialization
 	void Start () {
@@ -11,18 +20,39 @@ public class Gate : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
+		//after delay is over 
+		if ( Time.time - m_triggerCounter > m_triggerDelay )
+			m_isTriggerPressed = false;
+
+		bool trigger = m_isLeftTrigger ? InputManager.instance.m_isLeft : InputManager.instance.m_isRight;
+		if ( !m_isTriggerPressed && trigger )
+			Press();
 	
 	}
 
-	void OnTriggerEnter2D(Collider2D other) {
-		//isTrigger = true;
-	
+	void OnTriggerStay2D( Collider2D other ) 
+	{
+		if ( other.tag.Equals( "Ball" ) && m_isTriggerPressed )
+			StartCoroutine( DisableWallColliders( m_colliders ) );
 	}
 
-	void OnTriggerExit2D(Collider2D other) {
-		//isTrigger = false;
+	IEnumerator DisableWallColliders( Collider2D[] colliders )
+	{
+		foreach( Collider2D collider in colliders )
+			collider.enabled = false;
 		
+		yield return new WaitForSeconds( m_wallColliderDelay );
+		
+		foreach( Collider2D collider in colliders )
+			collider.enabled = true;
 	}
 
+	void Press()
+	{
+		m_isTriggerPressed = true;
+
+		m_triggerCounter = Time.time;
+	}
 }
---
+++ project/Assets/Scripts/Input/InputManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour 
+{
+	public static InputManager instance { get { return m_instance; } }
+
+	private static InputManager m_instance;
+
+	public bool m_isLeft;
+	public bool m_isRight;
+
+	public float m_swipeThreshold;
+
+	void Awake()
+	{
+		if ( m_instance == null )
+			m_instance = this;
+		else
+			Destroy( this );
+	}
+
+	void Update()
+	{
+		m_isLeft  = Input.GetButtonUp( "Left" );
+		m_isRight = Input.GetButtonUp( "Right" );
+	}
+
+	void OnDrag( Vector2 delta )
+	{
+		float drag = delta.x;
+		if ( Mathf.Abs( drag ) > m_swipeThreshold )
+		{
+			if   ( drag > 0 ) m_isRight = true;
+			else              m_isLeft  = true;
+		}
+	}
+}
