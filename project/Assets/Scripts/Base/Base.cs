fb747bad1e2ed4ffac9eb443b9a4712969d710c5
Vincent <vincentiu.adam@gmail.com>
2014-05-03 19:33:03 +0200

    Finished Track Setup
    Adapted Camera script to follow ball based on Local Position :D
    
    Added textures to Atlas

---
+++ project/Assets/Scripts/Base/Base.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Base : MonoBehaviour 
+{
+	public Transform cachedTransform
+	{
+		get 
+		{
+			if ( m_cachedTransform == null )
+				m_cachedTransform = transform;
+
+			return m_cachedTransform;
+		}
+	}	
+
+	private Transform m_cachedTransform;
+}
