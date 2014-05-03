6795c5f8c6b53564bab793e39e03e04dac7abd06
Emmanuel Corno <emmanuel.corno@neuf.fr>
2014-05-03 17:32:29 +0200

    Inactive power ups

---
+++ project/Assets/Scripts/PowerUp.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class PowerUp : MonoBehaviour {
+
+	public enum PowerType {
+		Left,
+		Right
+	};
+
+	public PowerType powerType = PowerType.Left;
+
+	// Use this for initialization
+	void Start () {
+		SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+
+		switch(powerType) {
+			case PowerType.Left:
+				sprite.sprite = Resources.Load<Sprite>("leftPower");
+			break;
+			case PowerType.Right:
+				sprite.sprite = Resources.Load<Sprite>("rightPower");
+			break;
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
