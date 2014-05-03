dde0e31767cc16ea12fb68556ae0905a6beacacc
Emmanuel Corno <emmanuel.corno@neuf.fr>
2014-05-03 12:34:43 +0200

    Stick

--- project/Assets/Scripts/Ball.cs
+++ project/Assets/Scripts/Ball.cs
@@ -5,28 +5,44 @@ public class Ball : MonoBehaviour {
 
 	public float horizontalSpeed = 5f;
 	public float verticalSpeed = 5f;
+	public float timeStick = 750f;
 
 	private Vector3 speedVectorLeft;
 	private Vector3 speedVectorRight;
 
 	private bool isGoingToLeft = true;
 
+	private bool isSticking = false;
+	private float timerSticking = 0f;
+
 	// Use this for initialization
 	void Start () {
 		speedVectorLeft = new Vector3(-horizontalSpeed, -verticalSpeed, 0);
 		speedVectorRight = new Vector3(horizontalSpeed, -verticalSpeed, 0);
 	}
+
+	void Update() {
+		if(isSticking) {
+			timerSticking += Time.deltaTime;
+			if(timerSticking >= timeStick)
+				isSticking = false;
+		}
+	}
 	
 	void FixedUpdate () {
-		Vector3 pos = transform.position;
-		if(isGoingToLeft)
-			pos += speedVectorLeft * Time.deltaTime;
-		else
-			pos += speedVectorRight * Time.deltaTime;
-		transform.position = pos;
+		if(!isSticking) {
+			Vector3 pos = transform.position;
+			if(isGoingToLeft)
+				pos += speedVectorLeft * Time.deltaTime;
+			else
+				pos += speedVectorRight * Time.deltaTime;
+			transform.position = pos;
+		}
 	}
 
 	void OnTriggerEnter2D(Collider2D other) {
 		isGoingToLeft = !isGoingToLeft;
+		timerSticking = 0f;
+		isSticking = true;
     }
 }
