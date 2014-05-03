655f85660ab8a1749561ea5e6a889dcb38e97e0d
Yoann Cribier <yoann_cribier@hotmail.com>
2014-05-03 19:54:14 +0200

    Added Menu (Scene & Script)

---
+++ project/Assets/Scripts/Menu.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+	public int highscore = 0;
+
+	public GameObject highscoreGO;
+	private UILabel highscoreScript;
+
+	// Use this for initialization
+	void Start () {
+
+		highscoreScript = highscoreGO.GetComponent<UILabel>();
+
+		if (PlayerPrefs.HasKey("Highscore"))
+			highscore = PlayerPrefs.GetInt("Highscore");
+		else
+			highscore = 0;
+		highscoreScript.text = "HIGHSCORE\n" + highscore.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void LoadGame() {
+		Application.LoadLevel("Game");
+	}
+
+	public void LoadCredits() {
+		//Application.LoadLevel("Credits");
+	}
+
+	public void LoadHowTo() {
+		//Application.LoadLevel("Howto");
+	}
+}
