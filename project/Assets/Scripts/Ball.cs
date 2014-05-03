61740ea9beddb5d4daeea0874d44a0028d5da753
Emmanuel Corno <emmanuel.corno@neuf.fr>
2014-05-03 11:19:25 +0200

    Movement!

---
+++ project/Assets/Scripts/Ball.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	public float speed = 5f;
+
+	private Vector3 speedVectorLeft;
+	private Vector3 speedVectorRight;
+
+	private bool isGoingToLeft = true;
+
+	// Use this for initialization
+	void Start () {
+		speedVectorLeft = -Vector3.up*0.5f*Mathf.PI - Vector3.right*0.5f*Mathf.PI;
+		speedVectorRight = -Vector3.up*0.5f*Mathf.PI + Vector3.right*0.5f*Mathf.PI;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = transform.position;
+		if(isGoingToLeft)
+			pos += speedVectorRight * Time.deltaTime * speed;
+		else
+			pos += speedVectorRight * Time.deltaTime * speed;
+		transform.position = pos;
+	}
+}
