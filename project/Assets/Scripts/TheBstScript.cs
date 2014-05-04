b6f0e38b9aa921b7fd6966618f15e3648fd91fe9
Clement Denis <cdenis@student.42.fr>
2014-05-04 15:39:42 +0200

    Flickering Background sound

---
+++ project/Assets/Scripts/TheBstScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TheBstScript : MonoBehaviour {
+
+    public float val; // sound level - dB
+    public float max = 11f;
+    public float min = -130f;
+    public Color baseColor;
+
+    private float[] samples; // audio samples
+    private float fSample;
+     
+    public UISprite _sprite;
+	// Use this for initialization
+	void Start () {
+        _sprite = GetComponent<UISprite>();
+        baseColor = _sprite.color;
+        samples = new float[1];
+        fSample = AudioSettings.outputSampleRate;
+        audio.Play();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        audio.GetOutputData(samples, 0); // fill array with samples
+        val = 20f * Mathf.Log10( samples[0] / 0.1f ); // calculate dB
+        if (val < -160f)
+            val = -160f; // clamp it to -160dB min
+        else {
+            if (val > max) {
+                max = val;
+            }
+            else if (val < min) {
+                min = val;
+            }
+            val = (max + min) / (val + min);
+
+            _sprite.color = Color.Lerp(Color.black, baseColor, val);
+        }
+	
+	}
+}
