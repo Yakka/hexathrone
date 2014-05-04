bb2d75565e1c71a351c55abaea157376e15a3273
Vincent <vincentiu.adam@gmail.com>
2014-05-04 15:19:42 +0200

    Fixed Input Manager for Mobile

--- project/Assets/Scripts/Input/InputManager.cs
+++ project/Assets/Scripts/Input/InputManager.cs
@@ -16,6 +16,8 @@ public class InputManager : MonoBehaviour
 
 	public float m_holdCounter;
 
+	private bool m_isHold;
+	
 	private float m_clickCounter;
 	void Awake()
 	{
@@ -27,26 +29,33 @@ public class InputManager : MonoBehaviour
 
 	void Update()
 	{
+#if UNITY_EDITOR || ( !UNITY_ANDROID && !UNITY_IPHONE )
+
 		m_isLeft  = Input.GetButtonUp( "Left" );
 		m_isRight = Input.GetButtonUp( "Right" );
 
-#if UNITY_EDITOR || ( !UNITY_ANDROID && !UNITY_IPHONE )
-
 		m_isTapped = Input.GetButtonUp( "Tap" );
+
+		m_isHold = Input.GetButton( "Tap" );
 
 		if ( Input.GetButtonDown( "Tap" ) )
 			m_holdCounter = Time.time;
 #else
 
 		if ( Time.time - m_clickCounter > m_clickDelay )
+		{
 			m_isTapped = false;
+
+			m_isRight = false;
+			m_isLeft  = false;
+		}
 #endif
 	}
 
 	void LateUpdate()
 	{
 		//reset hold
-		if ( !Input.GetButton( "Tap" ) )
+		if ( !m_isHold )
 			m_holdCounter = Time.time;
 	}
 
@@ -54,10 +63,13 @@ public class InputManager : MonoBehaviour
 	{
 		float drag = delta.x;
 	
+		Debug.Log( delta.x );
 		if ( Mathf.Abs( drag ) > m_swipeThreshold )
 		{
 			if   ( drag > 0 ) m_isRight = true;
 			else              m_isLeft  = true;
+
+			m_clickCounter = Time.time;
 		}
 	}
 
@@ -66,6 +78,13 @@ public class InputManager : MonoBehaviour
 		m_isTapped = true;
 
 		m_clickCounter = Time.time;
+	}
+
+	void OnPress( bool isPress )
+	{
+		m_isHold = isPress;
+		if ( isPress )
+			m_holdCounter = Time.time;
 	}
 
 
